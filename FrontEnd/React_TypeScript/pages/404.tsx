84e4f2de68ea8c9d85657c8b7e9410f22ad5be47
sungchan <tjdcksdl00@naver.com>
2020-10-22 19:33:10 +0900

    init setting

---
+++ FrontEnd/React_TypeScript/pages/404.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Error404 = (): JSX.Element => {
+    return (
+        <>
+            <div>
+                <p>404 에러 페이지</p>
+            </div>
+        </>
+    );
+};
+
+export default Error404;
